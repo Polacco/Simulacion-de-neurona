9c00fb089aef54f28d53a79665799d81c35e964f
Polacco <joaquinmpol7@gmail.com>
2026-04-14 14:41:21 -0300

    Primer dibujo - Ion de Sodio

--- C++/main.cpp
+++ C++/main.cpp
@@ -2,27 +2,34 @@
 #include <iostream>
 
 int main() {
-    if (!glfwInit()) {
-        std::cerr << "Error crítico: Fallo al inicializar GLFW." << std::endl;
-        return -1;
-    }
+    if (!glfwInit()) return -1;
 
     GLFWwindow* window = glfwCreateWindow(1024, 768, "Bio-Molecular Neuron - C++ Engine", NULL, NULL);
     if (!window) {
-        std::cerr << "Error crítico: Fallo al crear la ventana." << std::endl;
         glfwTerminate();
         return -1;
     }
 
     glfwMakeContextCurrent(window);
 
+    // BUCLE PRINCIPAL
     while (!glfwWindowShouldClose(window)) {
         
         glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glfwSwapBuffers(window);
         
+        glPointSize(15.0f); 
+
+        glBegin(GL_POINTS);
+            
+            glColor3f(1.0f, 1.0f, 0.0f); 
+            
+            glVertex2f(0.0f, 0.0f); 
+
+        glEnd();
+        
+        glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
